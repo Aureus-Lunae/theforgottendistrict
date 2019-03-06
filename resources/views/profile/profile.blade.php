735f3412d1def5ef2f9737a2ffa5a76cf78fa072
Erwin Korsten <mw.korsten@gmail.com>
2019-03-06 15:22:29 +0100

    profile css

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -5,21 +5,25 @@
 @section('content')
   <div class="main_container">
     <div class="profile">
-      <img src="/img/avatars/{{ $user->avatar }}" style="width: 256px; height: 256px; float:left; border-radius: 50%; margin-right: 32px;">
+      <img src="/img/avatars/{{ $user->avatar }}" class="avatar">
       <h2> {{ $user->name }}'s Profile </h2>
 
       <div class="joined">Joined: {{ date('d-m-Y', strtotime($user->created_at)) }}</div>
       <p class="descr">{!! nl2br(e($user->desc )) !!}</p>
     </div>
 
-    <div class="links"><a href="/profile/changePassword">Change Password</a></div>
-    <form enctype="multipart/form-data" action="/profile/avatar" method="POST">
-      {{ method_field('PATCH') }}
-      {{ csrf_field() }}
-      <label>Update Avatar:</label>
-      <input type="file" name="avatar">
-      <input type="submit" class="button" value="Change Avatar"/> 
-    </form>
+    <div class="profile_links">
+      <div class="links">
+        <a href="/profile/changePassword">Change Password</a>
+      </div>
+      <form enctype="multipart/form-data" action="/profile/avatar" method="POST">
+        {{ method_field('PATCH') }}
+        {{ csrf_field() }}
+        <label>Update Avatar:</label>
+        <input type="file" name="avatar">
+        <input type="submit" class="button" value="Change Avatar"/> 
+      </form>
+    </div>
   </div>
 
 
