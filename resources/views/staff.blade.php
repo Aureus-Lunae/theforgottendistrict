3ca385b2efdbfcde7943083a83c451c6c17cdc20
Erwin Korsten <mw.korsten@gmail.com>
2019-04-01 14:57:33 +0200

    Staff page now supports markdown

--- resources/views/staff.blade.php
+++ resources/views/staff.blade.php
@@ -3,12 +3,18 @@
 @section('content')
   <div class="staff_container">
     @foreach($staff as $staffMember)
-      <div class="card">
-        <h2 class="name"> {{ $staffMember->name }}</h2>
-        <div class="rank"> {{ $staffMember->RankName }}</div>
-        <div class="avatar"> <img src="/img/avatars/{{ $staffMember->avatar }}"> </div>
-        <div class="descr"> {!! nl2br(e($staffMember->desc )) !!} </div>
-      </div>
+      <a href="/users/{{ $staffMember->id }}">
+        <div class="card">
+          <h2 class="name"> {{ $staffMember->name }}</h2>
+          <div class="rank"> {{ $staffMember->RankName }}</div>
+          <div class="avatar"> <img src="/img/avatars/{{ $staffMember->avatar }}"> </div>
+          <div class="descr markdown">
+            @markdown
+{{$staffMember->desc}}
+            @endmarkdown
+          </div>
+        </div>
+      </a>
     @endforeach
   </div>
 @endsection
